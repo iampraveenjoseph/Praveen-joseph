591b252dccb152bcc90b5d668c632315e131b82b
Praveen joseph P <53596975+iampraveenjoseph@users.noreply.github.com>
2019-12-04 11:07:19 +0530

    Update colorlist.py

--- colorlist.py
+++ colorlist.py
@@ -1,5 +1,3 @@
 color_list = ["Red","Green","White" ,"Black"]
 print(color_list[0])
-print(color_list[-1])color_list = ["Red","Green","White" ,"Black"]
-print(color_list[0])
-print(color_list[-1])
+print(color_list[-1]
